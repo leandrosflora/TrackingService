9589584ad626673ea63255c1bb5899e74e8856f6
1513 X-MXTI <1513 X-MXTI@1513_X-MXTI>
2026-06-21 23:38:05 -0300

    Instrumentação opentelemetry

--- Program.cs
+++ Program.cs
@@ -7,7 +7,25 @@ using TrackingService.Infrastructure.Messaging;
 using TrackingService.Infrastructure.Outbox;
 using TrackingService.Infrastructure.Persistence;
 
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+var serviceName = builder.Environment.ApplicationName;
+var otlpEndpoint = builder.Configuration["OpenTelemetry:OtlpEndpoint"] ?? "http://localhost:5107";
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint)))
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddPrometheusExporter());
 
 builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
@@ -47,6 +65,8 @@ builder.Services.AddHealthChecks()
     .AddDbContextCheck<TrackingDbContext>();
 
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint("/metrics");
 
 app.UseExceptionHandler();
 
